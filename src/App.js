e801f51da4dd858c00402e7290eeef470c897e4e
Ahmet Burak OKUR <212439566+ABurakOKUR@users.noreply.github.com>
2026-02-05 15:21:07 +0300

    Hesap makinesi reducer görevleri tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useReducer } from "react"; //
+import reducer, { initialState } from "./reducers/indexReducer"; //
+import {
+  applyNumber,
+  changeOperation,
+  clearDisplay,
+  memoryPlus,
+  memoryRecall,
+  memoryClear,
+} from "./actions/index"; //
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState); //
+
+  const handleNumberClick = (num) => {
+    dispatch(applyNumber(num));
+  }; //
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -13,47 +28,63 @@ function App() {
       <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} /> {/* */}
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>{" "}
+              {/* */}
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>{" "}
+              {/* */}
             </div>
-
             <div className="row">
-              <CalcButton value={"M+"} />
-              <CalcButton value={"MR"} />
-              <CalcButton value={"MC"} />
+              <CalcButton value={"M+"} onClick={() => dispatch(memoryPlus())} />
+              <CalcButton
+                value={"MR"}
+                onClick={() => dispatch(memoryRecall())}
+              />
+              <CalcButton
+                value={"MC"}
+                onClick={() => dispatch(memoryClear())}
+              />
             </div>
-
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={1} onClick={() => handleNumberClick(1)} />
+              <CalcButton value={2} onClick={() => handleNumberClick(2)} />
+              <CalcButton value={3} onClick={() => handleNumberClick(3)} />
             </div>
-
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={() => handleNumberClick(4)} />
+              <CalcButton value={5} onClick={() => handleNumberClick(5)} />
+              <CalcButton value={6} onClick={() => handleNumberClick(6)} />
             </div>
-
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={() => handleNumberClick(7)} />
+              <CalcButton value={8} onClick={() => handleNumberClick(8)} />
+              <CalcButton value={9} onClick={() => handleNumberClick(9)} />
             </div>
-
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton
+                value={"+"}
+                onClick={() => dispatch(changeOperation("+"))}
+              />
+              <CalcButton
+                value={"*"}
+                onClick={() => dispatch(changeOperation("*"))}
+              />
+              <CalcButton
+                value={"-"}
+                onClick={() => dispatch(changeOperation("-"))}
+              />
             </div>
-
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton
+                value={"CE"}
+                onClick={() => dispatch(clearDisplay())}
+              />
             </div>
-
           </form>
         </div>
       </div>
---
+++ src/reducers/indexReducer.js
@@ -0,0 +1,75 @@
+import {
+  ADD_ONE,
+  APPLY_NUMBER,
+  CHANGE_OPERATION,
+  CLEAR_DISPLAY,
+  MEMORY_PLUS,
+  MEMORY_RECALL,
+  MEMORY_CLEAR,
+} from "./src/actions/index";
+
+export const initialState = {
+  total: 0,
+  operation: "+",
+  memory: 0,
+};
+
+const calculateResult = (num1, num2, operation) => {
+  switch (operation) {
+    case "+":
+      return num1 + num2;
+    case "*":
+      return num1 * num2;
+    case "-":
+      return num1 - num2;
+    default:
+      return;
+  }
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_ONE:
+      return {
+        ...state,
+        total: state.total + 1,
+      };
+
+    case APPLY_NUMBER:
+      return {
+        ...state,
+        total: calculateResult(state.total, action.payload, state.operation),
+      };
+
+    case CHANGE_OPERATION:
+      return {
+        ...state,
+        operation: action.payload,
+      };
+    case CLEAR_DISPLAY:
+      return {
+        ...state,
+        total: 0,
+      };
+    case MEMORY_PLUS:
+      return {
+        ...state,
+        memory: state.total,
+      };
+    case MEMORY_RECALL:
+      return {
+        ...state,
+        total: calculateResult(state.total, state.memory, state.operation),
+      };
+    case MEMORY_CLEAR:
+      return {
+        ...state,
+        memory: 0,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
